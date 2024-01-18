07b809458c40fabeecc4af6564ef265a2c12a80a
Sidarth2k <157009521+Sidarth2k@users.noreply.github.com>
2024-01-18 17:23:06 +0530

    Add files via upload

---
+++ EmailQuery.sql
@@ -0,0 +1,15 @@
+Select 
+[Met/Missed 1 Bus Day]
+,[Met/Missed 2 Bus Day]
+,[ReceivedOn]
+,[CloseDate]
+,[Subject]
+,[Sender]
+,Sum([AdjustedSLA]) as Adjusted_SLA
+From [Python_Task].[dbo].[Email_SLA]
+group by [Met/Missed 1 Bus Day]
+,[Met/Missed 2 Bus Day]
+,[ReceivedOn]
+,[CloseDate]
+,[Subject]
+,[Sender]
