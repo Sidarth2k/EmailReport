ed5c746a3264df92c8ae9c1a9472eff77205e75b
Sidarth2k <157009521+Sidarth2k@users.noreply.github.com>
2024-01-25 15:08:06 +0530

    EmailQuery.sql

--- EmailQuery.sql
+++ EmailQuery.sql
@@ -3,11 +3,9 @@ Select
 ,[Met/Missed 2 Bus Day]
 ,[ReceivedOn]
 ,[CloseDate]
-,[Subject]
 ,Sum([AdjustedSLA]) as Adjusted_SLA
 From [Python_Task].[dbo].[Email_SLA]
 group by [Met/Missed 1 Bus Day]
 ,[Met/Missed 2 Bus Day]
 ,[ReceivedOn]
 ,[CloseDate]
-,[Subject]
